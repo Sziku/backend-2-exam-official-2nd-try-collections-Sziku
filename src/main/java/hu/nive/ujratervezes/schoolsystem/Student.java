f9873d3a2688433f763a43311e4631419c7a3678
Sziku <105989158+Sziku@users.noreply.github.com>
2023-01-18 13:07:16 +0100

    create Student.java

--- src/main/java/hu/nive/ujratervezes/schoolsystem/Student.java
+++ src/main/java/hu/nive/ujratervezes/schoolsystem/Student.java
@@ -1,4 +1,17 @@
 package hu.nive.ujratervezes.schoolsystem;
 
+import java.time.LocalDate;
+import java.util.Set;
+
 public class Student {
+    private String name;
+    private LocalDate startDate;
+    Set<String> subjects;
+
+    public Student(String name, LocalDate startDate, Set<String> subjects) {
+        this.name = name;
+        this.startDate = startDate;
+        this.subjects = subjects;
+    }
+
 }
